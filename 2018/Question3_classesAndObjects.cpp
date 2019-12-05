c43822ccd03baf842964d9b94024fd4861f4a589
Bikramjit Narwal <bikramjit.narwal@mail.utoronto.ca>
2019-12-05 06:57:29 -0500

    third question

---
+++ 2018/Question3_classesAndObjects.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*
+
+#include "YetAnotherMystery . h"
+class Mystery {
+private:
+	YetAnotherMystery* mystery_variable;
+	-Mystery();
+public:
+	Mystery();
+	Mystery(const Mystery & other);
+	Mystery & operator=(const Mystery & rhs);
+	void operateOnMystery();
+	void print () const;
+
+Shannon wrote a nonmember function that has the following prototype:
+void mysterious ();
+
+The function creates a Mystery object and would like to delete the object to avoid a memory leak.
+However, it cannot because the destructor of Mystery is private. Amy wants to allow only
+Shannon's non-member function to delete Mystery objects but still prevent all other non-member
+functions from doing the same. She wants to do so without making Shannon's function a member of
+her class.
+
+Amy needs to add only one line to her mystery . h. No other modifications to mystery . h are
+allowed.
+Write the line Amy should add
+*/
+
+friend void mysterious (); // added as a friend 
