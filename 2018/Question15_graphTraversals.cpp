c08104ec4a400039f250db3ebb24c3f057e507a9
Bikramjit Narwal <bikramjit.narwal@mail.utoronto.ca>
2019-12-07 00:40:01 -0500

    question 15

---
+++ 2018/Question15_graphTraversals.cpp
@@ -0,0 +1,16 @@
+/* Consider the graph shown below. 
+ * 
+ *             2*
+ *           /   *3*
+ *         0     / |
+ *          \   /  |
+ *            1    4
+ * 
+ *  (The * shows the direction of the connection) 
+ * 
+ * Give a depth-first traversal of the graph, starting at node 0. Assume that visiting a
+ node means printing its number to the standard output. 
+ *  
+ *  Depth First Search (Use a stack) : 0 1 3 2 4 
+ *  Breadth First Search (Use a queue) : 0 2 1 3 4
+ *
