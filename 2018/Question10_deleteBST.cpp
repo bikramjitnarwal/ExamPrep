7a3cb83fe1d23ae2c0a0da3e5218e1dd1b778fb7
Bikramjit Narwal <bikramjit.narwal@mail.utoronto.ca>
2019-12-08 02:19:24 -0500

    Question 10

---
+++ 2018/Question10_deleteBST.cpp
@@ -0,0 +1,27 @@
+/*
+Assume the following simplified definition of TreeNode.
+ * 
+class TreeNode {
+public:
+    int key;
+	TreeNode *left;
+	TreeNode *right;
+};
+ * 
+Assume that your program creates a binary search tree with a pointer to its root node called root.
+Write the destructor of TreeNode so that the following statement deletes all the nodes in your tree
+(unlike your lab assignment).
+
+delete root; 
+ */
+TreeNode::~TreeNode(){ 
+	deleteBST(root);
+{
+
+void deleteBSR(TreeNode *root){
+	if(root == NULL) return;
+	
+	deleteBST(root->left);
+	deleteBST(root->left);
+	free(root);
+}
