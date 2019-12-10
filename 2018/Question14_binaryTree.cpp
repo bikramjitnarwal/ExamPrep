629a5479534f80ea95b1bc9b4f150e3a122b9fae
Bikramjit Narwal <bikramjitnarwal@hotmail.com>
2019-12-09 22:25:43 -0500

    Update Question14_binaryTree.cpp

--- 2018/Question14_binaryTree.cpp
+++ 2018/Question14_binaryTree.cpp
@@ -1,19 +1,19 @@
 /*
 Given a binary tree, you need to write a function to check if that tree is a mirror of itself. A tree is a
 mirror of itself if it is symmetric around its root.
- * 
+
 For example the binary tree on the left below is a mirror. In contrast, the binary tree on the right
 below is not a mirror. 
- *    
- *                      1                       1
- *                     / \                     / \
- *                    /   \                   /   \ 
- *                   2     2                 2     2
- *                  / \   / \                 \     \
- *                 4   7 7   4                 5     5
- * 
- * 
- * Given the class definition of a TreeNode shown below, write a recursive function isSyrnmetric
+  
+                      1                       1
+                     / \                     / \
+                    /   \                   /   \ 
+                   2     2                 2     2
+                  / \   / \                 \     \
+                 4   7 7   4                 5     5
+ 
+ 
+Given the class definition of a TreeNode shown below, write a recursive function isSyrnmetric
 that returns true if the tree is a mirror, otherwise, it returns false.
 	class TreeNode {
 	public:
